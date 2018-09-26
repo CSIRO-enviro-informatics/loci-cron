11e06f813e20bf2aa7fcd3c7ccf5f2d662e33f49
Nicholas Car <nicholas.car@csiro.au>
2018-09-27 09:51:57 +1000

    working

---
+++ assets.py
@@ -0,0 +1,11 @@
+ASSETS = [
+    {
+        'label': 'GN-NAF dataset',
+        'uris': [
+            {
+                'address': 'http://linked.data.gov.au/dataset/gnaf?_format=text/turtle',
+                'regex': 'rdfs:label \"GNAF ontology\"'
+            }
+        ]
+    }
+]
--- loci-cron.py
+++ loci-cron.py
@@ -2,23 +2,12 @@
 import requests
 import re
 import datetime
+import json
+import passwords as pwds
+import assets
 
-
-ASSETS = [
-    {
-        'label': 'GN-NAF dataset',
-        'uris': [
-            {
-                'address': 'http://linked.data.gov.au/dataset/gnaf?_format=text/turtle',
-                'regex': 'rdfs:label \"GNAF ontology\"'
-            }
-        ]
-    }
-]
 
 MESSAGES = []
-
-EMAIL_ADDRESSES = []
 
 
 def validate_endpoint_content(uri, pattern):
@@ -33,7 +22,7 @@ def validate_endpoint_content(uri, pattern):
         return False
 
 
-def add_validation_to_failures_list(asset_label, uri, pattern):
+def add_validation_failure_to_list(asset_label, uri, pattern):
     MESSAGES.append('Asset \'{}\' failed URI {} with pattern "{}"'.format(asset_label, uri, pattern))
 
 
@@ -44,65 +33,60 @@ def make_error_message(messages):
             '%Y-%m-%dT%H:%M'
         )
     )
-    message = message + '\n'.join(messages)
-    return message
+    return message + '\n'.join(messages)
 
 
-def send_email_via_mailjet(message, email_addresses, api_keys):
-    api_key = api_keys['MJ_APIKEY_PUBLIC']
-    api_secret = api_keys['MJ_APIKEY_PRIVATE']
+def send_email_via_mailjet(message, subject, to_addresses, secrets):
+    recipients = []
+    for to_address in to_addresses:
+        recipients.append({'Email': to_address, 'Name': ''})
     data = {
-        'FromEmail': 'loci-cron@csiro.au',
-        'FromName': 'LOC-I cron',
-        'Subject': 'LOC-I cron report for {}'.format(
-            datetime.datetime.strftime(
-                datetime.datetime.now(),
-                '%Y-%m-%dT%H:%M'
-            )
-        ),
-        'Text-part': message,
-        'Html-part': '',
-        'Recipients': [
+        'Messages': [
             {
-                'Email': ','.join(email_addresses)
+                'From': {
+                    'Email': 'nicholas.car@csiro.au',
+                    'Name': 'LCO-I cron bot'
+                },
+                'To': recipients,
+                'Subject': subject,
+                'TextPart': message
             }
         ]
     }
 
     r = requests.post(
-        'https://api.mailjet.com/v3/send',
-        data=data,
+        secrets['API_ENDPOINT'],
+        data=json.dumps(data),
         headers={'Content-Type': 'application/json'},
-        auth=(api_key, api_secret)
+        auth=(secrets['MJ_APIKEY_PUBLIC'], secrets['MJ_APIKEY_PRIVATE'])
     )
-    print(r.status_code)
-    print(r.content.decode('utf-8'))
+    if r.status_code == 200:
+        return True
+    else:
+        # print(r.content.decode('utf-8'))
+        return False
 
 
 if __name__ == '__main__':
-    for asset in ASSETS:
+    for asset in assets.ASSETS:
         print('validating ' + asset['label'])
         for uri in asset['uris']:
             print('uri: ' + uri['address'])
             if validate_endpoint_content(uri['address'], uri['regex']) is True:
-                # do nothing as thuis passes
+                # do nothing as this passes
                 pass
             else:
                 # the validation has failed so add to failures list
-                add_validation_to_failures_list(asset['label'], uri['address'], uri['regex'])
+                add_validation_failure_to_list(asset['label'], uri['address'], uri['regex'])
 
     # check for any failures and send to people
-    import passwords as passes
-
-    api_keys = {
-        'MJ_APIKEY_PUBLIC': passes.MJ_APIKEY_PUBLIC,
-        'MJ_APIKEY_PRIVATE': passes.MJ_APIKEY_PRIVATE
-    }
     if len(MESSAGES) > 0:
+        print('Failures')
         message = make_error_message(MESSAGES)
-        send_email_via_mailjet(message, EMAIL_ADDRESSES, api_keys)
+        send_email_via_mailjet(message, 'Failures', pwds.emails, pwds.secrets)
     else:
+        print('No failures')
         # no errors so send an 'OK' message to main maintainer only
-        send_email_via_mailjet('no errors for this report', ['nicholas.car@csiro.au'], api_keys)
+        send_email_via_mailjet('No errors for this report', 'No failures', pwds.emails, pwds.secrets)
 
     print('done')
