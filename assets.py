2a4f93d7b053c67daab6ffdb29accca77296e965
Nicholas Car <nicholas.car@csiro.au>
2018-09-27 12:02:35 +1000

    updated GNAF SPARQL Address count

--- assets.py
+++ assets.py
@@ -7,8 +7,8 @@ ASSETS = [
                 'regex': 'rdfs:label \"GNAF ontology\"'
             },
             {
-                'address': 'http://gnafld.net/sparql?query=PREFIX%20gnaf%3A%20%3Chttp%3A%2F%2Fgnafld.net%2Fdef%2Font%23Address%3E%20SELECT%20(COUNT(%3Fs)%20AS%20%3Fc)%20WHERE%20%7B%3Fs%20a%20gnaf%3AAddress%20.%7D',
-                'regex': 'value'  # to be changes to an actual number when there are Address objects in the triplestore
+                'address': 'http://gnafld.net/sparql?query=SELECT%20(COUNT(%3Fs)%20AS%20%3Fc)%20WHERE%20%7B%3Fs%20a%20%3Chttp%3A%2F%2Flinked.data.gov.au%2Fdef%2Fgnaf%23Address%3E%20.%7D',
+                'regex': '200743'
             }
         ]
     }
