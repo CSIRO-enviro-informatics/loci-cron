ba5a1bedd3efe5d2a14d9bee4b43708d4a6773a9
Nicholas Car <nicholas.car@csiro.au>
2018-10-29 15:28:19 +1000

    asset update

--- assets.py
+++ assets.py
@@ -4,7 +4,7 @@ ASSETS = [
         'uris': [
             {
                 'address': 'http://linked.data.gov.au/dataset/gnaf?_format=text/turtle',
-                'regex': 'dct:description \"A Linked Data API accessing all of the content of the GNAF in RDF \& HTML\"'
+                'regex': 'dct:title \"GNAF Dataset Distribution as Linked Data\"\^\^xsd:string ;'
             },
             {
                 'address': 'http://gnafld.net/sparql?query=SELECT(COUNT(%3Fs)AS%20%3Fc)%20WHERE%20%7B%20%3Fs%20a%20%3Chttp%3A%2F%2Flinked.data.gov.au%2Fdef%2Fgnaf%23Locality%3E%20.%20%7D',
